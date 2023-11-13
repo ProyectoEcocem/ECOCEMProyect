f33f95ab259593ba3f6f9722c0a3b486466950a0
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-11-13 02:35:06 -0500

    agregado reporte (equipo-fecha)

--- models/entidades/equipo.cs
+++ models/entidades/equipo.cs
@@ -2,6 +2,7 @@ public class Equipo
 {
     public int IdEquipo {get; set;}
     public int IdSede {get; set;}
-    public Sede Sede {get; set;} = null!
+    public Sede Sede {get; set;} = null!;
     public List<RoturaEquipo> RoturasEquipos {get; set;}
+    public List<Reporte> Reportes {get; set;}
 }

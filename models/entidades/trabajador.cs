513a4a2c82eba5e5ae627c3461c9191e303493c3
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-11-13 00:42:05 -0500

    agregada relacion obligatoria en trabajador respecto a sede

--- models/entidades/trabajador.cs
+++ models/entidades/trabajador.cs
@@ -3,5 +3,5 @@ public class Trabajador
     public int IdTrabajador {get; set;}
     public string NombreTrabajador {get; set;}
     public int IdSede {get; set;}
-    public Sede Sede {get; set;}
+    public Sede Sede {get; set;} = null!
 }

5674bda7c21eb68b536067f0784440d445da982e
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-11-13 23:07:00 -0500

    arregaldo nombre de FechaId

--- models/interrelaciones/ordenTrabajo.cs
+++ models/interrelaciones/ordenTrabajo.cs
@@ -3,5 +3,5 @@ public class OrdenTrabajo
    public int EquipoId {get; set;} 
    public int BrigadaId {get; set;}
    public int TrabajadorId {get; set;}
-   public DateTime IdFecha {get; set;}
+   public DateTime FechaId {get; set;}
 }

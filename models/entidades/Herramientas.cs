6eab56aeb245393e1bf0e15e2d4e76030a28bc5b
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2023-11-14 00:47:17 -0500

    agregada agregacion Herrmienta-MantNecesario

--- models/entidades/Herramientas.cs
+++ models/entidades/Herramientas.cs
@@ -6,4 +6,5 @@ public class Herramientas
 {
     public int HerramientasId { get; set; }
     public List<OrdenTrabajoHerramienta> OrdenTrabajoHerramientas {get; } = new();
+    public List<HerramientaMantNecesario> HerramientaMantNecesarios {get; } = new();
 }
---
+++ models/interrelaciones/HerramientaMantNecesario.cs
@@ -0,0 +1,19 @@
+using ECOCEMProyect;
+
+namespace ECOCEMProyect;
+
+public class HerramientaMantNecesario
+{
+    public int HerramientasId {get; set;}
+
+    //llaves de Mantenimiento necesario
+    public int TipoEquipoId {get; set;}
+    public int AMId {get; set;}
+    public double HorasExpId {get; set; }
+
+    public Herramientas Herramienta {get; set;}
+    public MantenimientoNecesario MantenimientoNecesario {get; set;}
+
+    public string UnidadMedidaR {get; set;}
+    public int CantidadR {get; set;}
+}

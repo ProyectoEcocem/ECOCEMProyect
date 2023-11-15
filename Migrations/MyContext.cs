387acf77474b3fbf738364c193ed6b09e813d96e
Sherlyn <sherlynballestero@gmail.com>
2023-11-14 21:48:55 -0500

    api fluent

--- Migrations/MyContext.cs
+++ Migrations/MyContext.cs
@@ -52,18 +52,9 @@ public class MyContext : DbContext//IdentityDbContext<Trabajador, Role, Guid>
 
 
 
-
-
-
-
-
-
-
-
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Compra>().HasKey(key => new { key.SedeId, key.FabricaId, key.FechaId });
+       modelBuilder.Entity<Compra>().HasKey(key => new { key.SedeId, key.FabricaId, key.FechaId });
         modelBuilder.Entity<Venta>().HasKey(key => new { key.SedeId, key.EntidadCompradoraId, key.FechaId });
         modelBuilder.Entity<Carga>().HasKey(key => new { key.TipoCementoId, key.SiloId,key.VehiculoId, key.FechaId });
         modelBuilder.Entity<Descarga>().HasKey(key => new {key.TipoCementoId, key.SiloId, key.VehiculoId, key.FechaId});
@@ -71,8 +62,9 @@ public class MyContext : DbContext//IdentityDbContext<Trabajador, Role, Guid>
         modelBuilder.Entity<MedicionSilo>().HasKey(key => new { key.SiloId, key.MedidorId, key.FechaId });
         modelBuilder.Entity<MantenimientoNecesario>().HasKey(key => new { key.TipoEquipoId, key.AMId, key.HorasExpId });
         modelBuilder.Entity<OrdenTrabajo>().HasKey(key => new { key.EquipoId, key.BrigadaId, key.TrabajadorId, key.FechaId });
-        //modelBuilder.Entity<OrdenTrabajoAtendida>().HasKey(key => new { key.TipoEquipoId, key.AMId, key.HorasExpId };
+        modelBuilder.Entity<OrdenTrabajoAtendida>().HasKey(key => new { key.TipoEquipoId, key.AMId, key.HorasExpId };
         modelBuilder.Entity<RoturaEquipo>().HasKey(key => new { key.EquipoId, key.RoturaId, key.FechaId });
+         modelBuilder.Entity<Reporte>().HasKey(key => new { key.EquipoId, key.FechaId });
     
     
     }

ed818d9fa16c11f6254ce2340c3a16b5dd2e1248
Sherlyn <sherlynballestero@gmail.com>
2024-02-07 01:09:23 -0500

    silo completo_y funcional

--- Services/entidades/SiloServicio.cs
+++ Services/entidades/SiloServicio.cs
@@ -5,10 +5,14 @@ namespace ECOCEMProject;
 public class SiloServicio
 {
     private readonly MyContext _context;
+    private readonly TipoEquipoServicio _tipoEServicio;
+    private readonly EquipoServicio _equipoServicio;
 
-    public SiloServicio(MyContext context)
+    public SiloServicio(MyContext context, TipoEquipoServicio tipoEServicio,EquipoServicio equipoServicio)
     {
         _context = context;
+        _tipoEServicio=tipoEServicio;
+        _equipoServicio=equipoServicio;
     }
 
     public async Task<Silo> Get(int id)
@@ -40,15 +44,38 @@ public class SiloServicio
 
     public async Task<Silo> Create(SiloData silo)
     {
-       /* Silo t1 = new Silo();
+       if(_context.Silos.Any(elemento => elemento.NoSilo == silo.NoSilo))
+            return null!;
 
-        t1.SiloId = silo.SiloId;
-        t1.EquipoId = silo.EquipoId;
+        Silo s1 = new Silo();
+        EquipoData e = new EquipoData();
+        s1.altura=silo.altura;
+        s1.NoSede=silo.NoSede;
+        s1.radio=silo.radio;
+        s1.SiloId=silo.SiloId;
+        s1.NoSilo=silo.NoSilo;
 
-        _context.Silos.Add(t1);
+        e.EquipoId = silo.SiloId;
+        e.SedeId = silo.NoSede;
+        //falta ponerle el tipo de equipo
+        if(await _context.TiposEquipos.AnyAsync(elemento => elemento.TipoE == "silo")) 
+        {
+            e.TipoEId =  _context.TiposEquipos.First(elem => elem.TipoE == "silo").TipoEId;
+            await _equipoServicio.Create(e); 
+        }
+        else{
+           TipoEData tipoSilo=new TipoEData();
+           tipoSilo.TipoE="silo";
+           tipoSilo.TipoEId=0;
+           TipoEquipo TE = await _tipoEServicio.Create(tipoSilo);
+           e.TipoEId=TE.TipoEId;
+           await _equipoServicio.Create(e);
+           //cuando se borre ese silo se debe borrar ese equipo
+        }
+           
+        _context.Silos.Add(s1);
         await _context.SaveChangesAsync();
-        return t1;*/
-        return null;
+        return s1;
     }
 
     public async Task Delete(int id)

6f5e99b844f68a9b02f966a1bbfb40fb5c4dad6f
Kitsulee <lauradi1410@gmail.com>
2024-02-07 10:45:53 -0500

    Agregada Vista para Visualizar Acciones de Mantenimiento

---
+++ ui/src/components/VisualizarAccionmantenimiento.jsx
@@ -0,0 +1,88 @@
+
+import React from 'react';
+import InsertarAccionMantenimiento from './InsertarAccionMantenimiento';
+import {
+  Input,
+  Button,
+  Flex,
+  FormLabel,
+  Select,
+  Table,
+  TableContainer,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Modal,
+  AbsoluteCenter,
+  //BackgroundImage
+} from "@chakra-ui/react";
+import axios from 'axios';
+export default class VisualizarAccionMantenimiento extends React.Component {
+    state = {
+      accionesMantenimiento: [],
+      insertarAccionMantenimientoEModalAbierto: false,
+    }
+  
+    componentDidMount() {
+      axios.get(`http://localhost:5103/api/AccionMantenimiento`)
+        .then(res => {
+          const accionesMantenimiento= res.data;
+          this.setState({ accionesMantenimiento });
+        })
+    }
+
+    manejarInsertarAccionMantenimientoModal = () => {
+      this.setState({ insertarAccionMantenimientoEModalAbierto: true });
+    };
+  
+    render() {
+      return (
+        <div style={{height : 400}}>
+
+<Button
+         onClick={this. manejarInsertarAccionMantenimientoModal}
+         marginBottom={5}
+         marginTop={5}
+         >
+          Agregar Accion de Mantenimiento
+         </Button>
+
+         <Modal isOpen={this.state.insertarAccionMantenimientoEModalAbierto} onClose={() => this.setState({ insertarAccionMantenimientoEModalAbierto: false })}>
+         <AbsoluteCenter>
+         <InsertarAccionMantenimiento onClose={() => this.setState({ insertarAccionMantenimientoEModalAbierto: false })} />
+         </AbsoluteCenter>
+         </Modal>
+
+        <TableContainer>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>ID de la Brigada</Th>
+                <Th>ID del Equipo</Th>
+                <Th>ID del Trabajador</Th>
+                <Th>Fecha</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {
+                this.state.accionesMantenimiento.map((accionMantenimiento) => (
+                  <Tr key={accionMantenimiento.brigadaId}>
+                    <Td>{accionMantenimiento.brigadaId}</Td>
+                    <Td>{accionMantenimiento.equipoId}</Td>
+                    <Td>{accionMantenimiento.trabajadorId}</Td>
+                    <Td>{accionMantenimiento.fechaId}</Td>
+                  </Tr>
+                ))
+              }
+            </Tbody>
+          </Table>
+        </TableContainer>
+        </div>
+        // <ul>
+          // { this.state.roturasE.map(roturaE => <li key={roturaE.roturaId}> Id:{roturaE.roturaId} Equipo: {roturaE.equipoId} Fecha:  Equipo: {roturaE.fechaId} </li>)}
+        // </ul>
+      )
+    }
+  } 
